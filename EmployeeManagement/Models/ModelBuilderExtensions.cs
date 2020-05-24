3305309026c6e99799b1551b6808d69e0066a87d
Vipula Rohini Mulugu <vipula11@outlook.com>
2020-05-25 01:08:50 +0530

    Employee Management Code - 25/05

---
+++ EmployeeManagement/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Blue",
+                    Department = Dept.Administration,
+                    Email = "blue@gmail.com"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Charlie",
+                    Department = Dept.Execution,
+                    Email = "charlie@gmail.com"
+                }
+                );
+        }
+    }
+}
---
+++ EmployeeManagement/ViewModels/EmployeeCreateViewModel.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeeCreateViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 0 characters")]
+        [RegularExpression(@"[a-zA-Z0-9_.+=]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid Email Format")]
+        [Display(Name = "Office Email")]
+        public string Email { get; set; }
+        [Required]
+        public Dept? Department { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
